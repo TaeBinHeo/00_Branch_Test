171c88152f738c811f63061aee3068a73f348ab9
TaeBinHeo <93439030+TaeBinHeo@users.noreply.github.com>
2021-10-30 23:31:29 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "main.h"
+
+int main(void)
+{
+	RS232_Communication* RS232 = new RS232_Communication;
+
+	CString RS232_Message;
+
+	RS232_Message.Format(_T("COM 16 Open"));
+
+	RS232->RS232_115200_Open(16);
+
+	while (1)
+	{
+		RS232->RS232_Send(RS232_Message, RS232_Message.GetLength());
+		Sleep(1000);
+		RS232->RS232_Read();
+	}
+
+	delete RS232;
+
+	return 0;
+}
